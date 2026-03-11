dc20e5af51412a1a3ddd06570b9010be0baa362a
Angel Ramirez <angelgabriel.16ra@gmail.com>
2026-03-10 22:36:03 -0600

    feat: add storybook configuration
    - add folder in .gitignore
    - add .ts file to handle environment variables
    - configure metro to view outside its default range
    - add scripts to storybook

--- apps/demo/src/app/_layout.tsx
+++ apps/demo/src/app/_layout.tsx
@@ -4,13 +4,28 @@ import {
 	ThemeProvider,
 } from "@react-navigation/native";
 import React from "react";
-import { useColorScheme } from "react-native";
+import { useColorScheme, View } from "react-native";
 
 import { AnimatedSplashOverlay } from "@/components/animated-icon";
 import AppTabs from "@/components/app-tabs";
+import { STORYBOOK_ENABLED } from "../../env";
 
-export default function TabLayout() {
+let StorybookUIRoot: any = null;
+if (STORYBOOK_ENABLED) {
+	StorybookUIRoot = require("../../.storybook").default;
+}
+
+export default function RootLayout() {
 	const colorScheme = useColorScheme();
+
+	if (STORYBOOK_ENABLED && StorybookUIRoot) {
+		return (
+			<View style={{ flex: 1 }}>
+				<StorybookUIRoot />
+			</View>
+		);
+	}
+
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
 			<AnimatedSplashOverlay />
